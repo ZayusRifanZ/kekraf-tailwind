2d4f6e362082d11e1773ffe267e7b9830723ce90
ZayusRifanZ <rifanmidai99@gmail.com>
2022-11-23 18:11:35 +0700

    fixed navbar scrolling function

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -8,6 +8,11 @@ humbergerIcon.addEventListener("click", mobileMenu);
 function mobileMenu() {
   humbergerIcon.classList.toggle("active");
   navMenu.classList.toggle("active");
+
+  // for trigger button humberger
+  if (window.innerWidth < 768) {
+    navTop.classList.add("scrolled");
+  }
 }
 
 // function scroll navbar active class ".scrolled"
@@ -16,7 +21,7 @@ window.onscroll = function () {
 };
 
 function scrolledFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     navTop.classList.add("scrolled");
   } else {
     if (navMenu.classList.contains("active") == false) {
